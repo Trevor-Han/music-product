8e5101dbab0c817c7689799b6f5ce945da7a02eb
han-bin520 <1263256614@qqcom>
2020-07-02 00:19:45 +0800

    公共底部模块封装

---
+++ common/js/footer.js
@@ -0,0 +1,17 @@
+$(function () {
+    /*公共底部处理*/
+    let pageArray = ["home", "video", "me", "friend", "account"];
+    $(".footer>ul>li").click(function () {
+        // 切换底部图标
+        $(this).addClass("active").siblings().removeClass("active");
+        let url = $(this).find("img").attr("src");
+        url = url.replace("normal", "selected");
+        $(this).find("img").attr("src", url);
+        $(this).siblings().find("img").forEach(function (oImg) {
+            oImg.src = oImg.src.replace("selected", "normal");
+        });
+        // 切换头部样式
+        let currentName = pageArray[$(this).index()];
+        $(".header").removeClass().addClass("header "+ currentName);
+    });
+});
--- index/js/index.js
+++ index/js/index.js
@@ -118,23 +118,7 @@ $(function () {
       $(".current-top").text(`搜索"${this.value}"`);
       searchScroll.refresh();
   },1000);
-
-  /*公共底部处理*/
-  let pageArray = ["home", "video", "me", "friend", "account"];
-  $(".footer>ul>li").click(function () {
-    // 切换底部图标
-    $(this).addClass("active").siblings().removeClass("active");
-    let url = $(this).find("img").attr("src");
-    url = url.replace("normal", "selected");
-    $(this).find("img").attr("src", url);
-    $(this).siblings().find("img").forEach(function (oImg) {
-      oImg.src = oImg.src.replace("selected", "normal");
-    });
-    // 切换头部样式
-    let currentName = pageArray[$(this).index()];
-    $(".header").removeClass().addClass("header "+ currentName);
-  });
-
+  
     /*处理公共的内容区域*/
     // 1.获取SVG路径的长度
     let length = $("#refreshLogo")[0].getTotalLength();
