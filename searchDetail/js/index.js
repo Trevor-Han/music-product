be077268d90d8eb28cd0a7a7143b7d44a0e475eb
Han-Bin520 <1263256614@qq.com>
2020-07-03 17:29:11 +0800

    完成单曲播放界面搭建、歌曲导航栏、歌曲全选

--- searchDetail/js/index.js
+++ searchDetail/js/index.js
@@ -13,6 +13,21 @@ $(function () {
     });
     $(".nav>ul>span").css({width: $(".nav>ul>li")[0].offsetWidth});
     $(".nav>ul>li").click(function () {
+        //计算偏移位
+        let offsetX = $(".nav").width() / 2 - this.offsetLeft - this.offsetWidth / 2;
+      /*  console.log(offsetX);
+        console.log(navScroll.x);
+        console.log(navScroll.maxScrollX);*/
+      if (offsetX > 0){
+          offsetX = 0;
+      }else if(offsetX < navScroll.maxScrollX){
+          offsetX = navScroll.maxScrollX;
+      }
+      //让导航条滚动
+        navScroll.scrollTo(offsetX,0,400);
+      //设置选中状态
+        $(this).addClass("active").siblings().removeClass("active");
+        $(".main-in>div").removeClass("active").eq($(this).index()).addClass("active");
        $(".nav>ul>span").animate({left: this.offsetLeft, width: this.offsetWidth},400);
     });
     /*公共底部处理*/
@@ -21,5 +36,19 @@ $(function () {
         let sc = document.createElement("script");
         sc.src = "./../common/js/footer.js";
         document.body.appendChild(sc);
+    });
+
+    /*处理公共的内容区域*/
+    $(".multiple-select").click(function () {
+       $(".single-music>.top").addClass("active");
+        $(".single-music>.bottom").addClass("active");
+    });
+    $(".complete-select").click(function () {
+        $(".single-music>.top").removeClass("active");
+        $(".single-music>.bottom").removeClass("active");
+    });
+    $(".check-all").click(function () {
+        $(this).toggleClass("active");
+        $(".single-music>.bottom>li").toggleClass("active");
     });
 });
